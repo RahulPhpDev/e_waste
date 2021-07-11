4dd408bc5e6b9b51680d42134cf52625eb4c5d6f
Rahul <mrrahul2016@gmail.com>
2021-07-11 21:17:28 +0530

    sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -3,7 +3,7 @@
 <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light sidenav-active-square">
     <div class="brand-sidebar">
         <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="/">
-                <span class="logo-text hide-on-med-and-down">Materialize</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
+                <span class="logo-text hide-on-med-and-down">{{ config('app.name', 'Laravel') }}</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
     </div>
 
 
