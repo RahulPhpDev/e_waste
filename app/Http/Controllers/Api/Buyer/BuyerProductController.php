849de86620a85cbce70e55274d10692b1ee0474e
Rahul <mrrahul2016@gmail.com>
2022-01-22 22:26:45 +0530

    change password and activ emenut

--- app/Http/Controllers/Api/Buyer/BuyerProductController.php
+++ app/Http/Controllers/Api/Buyer/BuyerProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CategoryResource;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -10,7 +11,8 @@ use Illuminate\Http\Request;
 class BuyerProductController extends Controller
 {
    public function index() {
-       $catgory = Category::pluck( 'name', 'id');
+
+       $catgory = CategoryResource::collection(Category::with('image')->get());
 
        return collect([
            'category' => $catgory,
